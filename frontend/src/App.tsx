811af4af05d773193e650174358b36df597bbdc4
Sam Gabo <sam.gabo@bld.ai>
2022-10-08 01:31:19 +0800

    Initial react installation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Homepage from './routes/homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <text>Hello</text>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <text>HomeScreen</text>
+    </div>
+  )
+}
+
+export default Homepage;
